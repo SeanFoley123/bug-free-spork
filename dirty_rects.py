70566b20516accfab78f03b6705a38dc7c492bbe
SeanFoley123 <sean.foley@students.olin.edu>
2016-03-28 23:05:11 -0400

    MMmmmmm

--- dirty_rects.py
+++ dirty_rects.py
@@ -8,7 +8,7 @@ from pygame.locals import *
 class Room(object):
 	### Assumes you make a self.objects_list, a self.start_positions, and a self.hero in your __init__. 
 	def update(self):
-		for thing in self.objects_list:
+		for thing in self.objects_list[0] + self.objects_list[1]:
 			thing.update()
 
 	def enter_room(self, direction):
@@ -22,14 +22,15 @@ class Room1(Room):
 	### keep track of their own position, a room size, and a background picture. Which should have clouds. And trees.
 	def __init__(self, hero):
 		self.hero = hero
-		self.room_size = (3000, 1000)
-		self.background = pygame.Surface(self.room_size)
+		self.room_size = (1000, 600)
+		self.background = pygame.transform.scale(pygame.image.load('dog.jpg'), self.room_size)
 		# Color chart at https://sites.google.com/site/meticulosslacker/pygame-thecolors
-		self.background.fill(pygame.Color('deepskyblue'))
-		self.background.fill(pygame.Color('lightsalmon3'), pygame.Rect(0, self.room_size[1]-200, self.room_size[0], 200))
 		# This sets start positions based on how big the room is, where the ground is (200 from bottom), and how big the hero is.
 		self.start_positions = [(self.room_size[0]-100, self.room_size[1]-200-self.hero.rect.h/2), (100, self.room_size[1]-200-self.hero.rect.h/2)]
-		self.objects_list = [self.hero]
+		# A list of lists of objects in the room. objects_list[0] is non-solid objects, objects_list[1] are solid.
+		self.objects_list = [[self.hero, Lava(pygame.Rect(self.room_size[0]/3, self.room_size[1]-210, self.room_size[0]/3, 210))],
+								[Ground(pygame.Rect(0, self.room_size[1]-200, self.room_size[0]/3, 200)), 
+								Ground(pygame.Rect(2*self.room_size[0]/3, self.room_size[1]-200, self.room_size[0]/3, 200))]]
 
 
 class Room2(Room):
@@ -37,24 +38,58 @@ class Room2(Room):
 		pass	
 
 
-class Walking_things(object):
+class Obstacle(object):
+	def __init__(self, rect, sprite, mortality = False, hardness = False):
+		self.hardness = hardness
+		self.mortality = mortality
+		self.sprite = sprite
+		self.rect = rect
+	
+	def update(self):
+		pass
+
+
+class Ground(Obstacle):
+	def __init__(self, rect):
+		sprite = pygame.Surface((rect.w, rect.h))
+		sprite.fill(pygame.Color('aquamarine4'))
+		Obstacle.__init__(self, rect, sprite, False, True)
+
+		
+class Lava(Obstacle):
+	def __init__(self, rect):
+		sprite = pygame.Surface((rect.w, rect.h))
+		sprite.fill(pygame.Color('chocolate1'))
+		Obstacle.__init__(self, rect, sprite, True, False)
+
+
+class WalkingThings(object):
 	###Defines basic movement for things that can move/jump
 	def walk(self, direction):
 		# print 'walking'
 		self.position = (self.position[0]+direction*self.speed, self.position[1])
 
 	def update(self):
-		self.moved = False
-		if self.position != self.rect.center:
-			# print 'i moved from ', self.rect.center, ' to ', self.position
-
-			self.model.dirty_rects.append(pygame.Rect(self.rect))
-			self.moved = True
 		self.rect.center = self.position
-			# print self.rect.center
+		self.check_collisions()
+
+	def check_collisions(self):
+		# I think we only care about collisions with non-solid things. We shouldn't actually ever overlap with a solid object.
+		for thing in self.model.current_room.objects_list[0]:
+			if self.rect.colliderect(thing.rect):
+				if thing.mortality:
+					self.die()
+
+	def is_clear(self, direction):
+		# Direction is vertical or horizontal
+		# Checks if there's a solid object at the rectangle shifted by vx or vy, whichever is appropriate
+		pass
+
+	def die(self):
+		self.model.current_room.objects_list[0].remove(self)
 
 
-class Mushroom_Guy(Walking_things):
+class MushroomGuy(WalkingThings):
 	### He moves, he shoots spores. I guess he needs to decide if he's just a funky little furry guy or a guy with a
 	### mushroom.
 	def __init__(self, model):
@@ -64,6 +99,12 @@ class Mushroom_Guy(Walking_things):
 		self.position = (self.rect.centerx, self.rect.centery)
 		self.moved = False
 		self.speed = 10 #how fast you walk
+		self.sprite = pygame.image.load('dog.jpg')
+		self.sprite = pygame.transform.scale(self.sprite, (100, 75))
+		self.mortality = False
+
+	def die(self):
+		self.sprite.fill(pygame.Color('red'), pygame.Rect(0, self.rect.h/2, self.rect.w, self.rect.h/2))
 
 
 class View(object):
@@ -76,35 +117,11 @@ class View(object):
 		self.screen = pygame.display.set_mode(screen_size)
 		self.position = (0, self.model.current_room.room_size[1] - self.screen_size[1])
 
-	def update(self, dirty_rects):
-		# if dirty_rects:
-		# 	print dirty_rects
-		for old_rect in dirty_rects: #this should only contain the rectangles that had something in them last step but need to be drawn over now
-			absolute_rect = old_rect.copy()
-			old_rect.topleft = old_rect.left - self.position[0], old_rect.top - self.position[1]
-			# print durr
-			self.screen.blit(self.model.current_room.background, old_rect, absolute_rect)
-			# pygame.draw.rect(self.screen,  pygame.Color('deepskyblue'), durr)
-		for thing in self.model.current_room.objects_list:
-			#This seems super inefficient; creates a new rectangle to draw every step.
-			if thing.moved:
-				# print 'hi'
-				# print dirty_rects
-				# print thing.rect
-				# print
-				place = pygame.Rect(thing.rect.left-self.position[0], thing.rect.top-self.position[1], thing.rect.w, thing.rect.h)    #placing it on the screen
-				print 'old_rect: ' , old_rect
-				print 'place: ', place
-				print
-				dirty_rects.append(place)
-				pygame.draw.rect(self.screen, pygame.Color('orange'), place)
-				print dirty_rects
-		pygame.display.update(dirty_rects)
-
-	def enter_room(self):
+	def update(self):
+		self.position = min(max(self.model.hero.position[0] - self.screen_size[0]/2, 0), self.model.current_room.room_size[0]-self.screen_size[0]), self.position[1]
 		self.screen.blit(self.model.current_room.background, (-self.position[0], -self.position[1]))
-		for thing in self.model.current_room.objects_list:
-			pygame.draw.rect(self.screen, pygame.Color('orange'), pygame.Rect(thing.rect.left-self.position[0], thing.rect.top-self.position[1], thing.rect.w, thing.rect.h))
+		for thing in self.model.current_room.objects_list[0] + self.model.current_room.objects_list[1]:
+			self.screen.blit(thing.sprite, (thing.rect.left-self.position[0], thing.rect.top-self.position[1]))
 		pygame.display.update()
 
 
@@ -117,7 +134,7 @@ class Controller(object):
 		if self.walking:
 			self.model.hero.walk(self.walking)
 		for event in pygame.event.get():
-			if event.type == QUIT:
+			if event.type == QUIT or event.type == KEYDOWN and event.key == pygame.K_ESCAPE:
 				pygame.quit()
 				sys.exit()
 			elif event.type == KEYDOWN and event.key == pygame.K_RIGHT:
@@ -128,6 +145,11 @@ class Controller(object):
 				self.walking -= 1
 			elif event.type == KEYUP and event.key == pygame.K_LEFT:
 				self.walking += 1
+			elif event.type == KEYDOWN and event.key == pygame.K_a:
+				self.model.view.position = self.model.view.position[0] - 100, self.model.view.position[1]
+			elif event.type == KEYDOWN and event.key == pygame.K_d:
+				self.model.view.position = self.model.view.position[0] + 100, self.model.view.position[1]
+
 
 class Model(object):
 	### Contains the controller, the view, the current room, and the mushroom guy. Keeps track of the current room.
@@ -138,7 +160,7 @@ class Model(object):
 		# Need a mushroom guy
 		# Need a current room
 		# Need a viewer of the proper size and a controller
-		self.hero = Mushroom_Guy(self)
+		self.hero = MushroomGuy(self)
 
 		self.room_list = [Room1(self.hero), Room2(self.hero)]
 		self.current_room = self.room_list[0]
@@ -148,21 +170,17 @@ class Model(object):
 
 		screen_size = (800, 600)
 		self.view = View(self, screen_size)
-		self.view.enter_room()
-		self.dirty_rects = []
 
 	def update(self):
 		self.controller.update()
 		self.current_room.update()
-		self.view.update(self.dirty_rects)
-		self.dirty_rects = []
+		self.view.update()
 
 	def change_room(self, direction):
 		### You try to move into the room that's in the direction you want to go: -1 means going left (so entering from the right), 
 		### and 1 means going right.
 		try:
 			self.current_room = self.room_list.index(self.current_room) + direction
-			self.current_room.enter_room(direction)
 		except:
 			print "You can't go there! What have you done you foolish programmer? You've doomed us all!"
 			pygame.quit()
