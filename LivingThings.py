07144f9c3f6b300f375c7fe3e55ed6f88074e7ef
Kaitlyn Keil <kaitlyn.keil@students.olin.edu>
2016-04-30 15:06:41 -0400

    Fixed the player image and made friends worse to kill

--- LivingThings.py
+++ LivingThings.py
@@ -82,13 +82,14 @@ class MushroomGuy(Living):
  
     # Constructor function
     def __init__(self):
+        self.size = (75, 75)
         # Call the parent's constructor
-        Living.__init__(self, 0, 0, 100, 75, 'png/mg_tc0.png', 0)
+        Living.__init__(self, 0, 0, self.size[0], self.size[1], 'png/mg_tc0.png', 0)
  
         # Set height, width
         self.image_list = [pygame.image.load('png/mg_tc0.png').convert_alpha(), pygame.image.load('png/mg_tc1.png').convert_alpha(), pygame.image.load('png/mg_tc2.png').convert_alpha()]
         for index, image in enumerate(self.image_list):
-            self.image_list[index] = pygame.transform.scale(image, (75, 75))
+            self.image_list[index] = pygame.transform.scale(image, self.size)
 
         self.speed = 6
 
@@ -151,7 +152,10 @@ class MushroomGuy(Living):
 
     def how_corrupt(self):
         """ Changes the image based on the corruption level of the player. """
-        self.image = pygame.transform.scale(self.image_list[self.corruption/5], (100, 75))
+        list_index = self.corruption/5
+        if list_index > len(self.image_list) - 1:
+            list_index = len(self.image_list) - 1
+        self.image = self.image_list[list_index]
 
     def draw_flipped(self):
         """Flips the player's sprite based on the value assigned to self.flipped (controlled by keypress)"""
@@ -318,13 +322,13 @@ class Friend(Enemy):
         self.image = pygame.image.load(image_file_name).convert_alpha()
         self.image = pygame.transform.scale(self.image, (width, height))
 
-class Adult_Duck(Friend):
+class AdultDuck(Friend):
     def __init__(self, x, y, width = 100, height = 100):
         self.width = width
         self.height = height
         Friend.__init__(self, x, y, self.width, self.height, "png/adult_duck.png")
 
-class Child_Duck(Friend):
+class ChildDuck(Friend):
     def __init__(self, x, y, width = 75, height = 75):
         self.width = width
         self.height = height
@@ -348,7 +352,7 @@ class Edible(pygame.sprite.Sprite):
         self.rect.y = y
         self.rect.x = x
 
-class Friend_Edible(Edible):
+class FriendEdible(Edible):
     """ This is an edible food that only comes from when you have killed a friendly creature. """
     def __init__(self, x, y, width, height):
         Edible.__init__(self, x, y, width, height, 5)
